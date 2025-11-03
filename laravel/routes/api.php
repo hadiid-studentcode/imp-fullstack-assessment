fd84e4a8d75eca9dfbc6ee1c0015301cdf47a7a0
Hadiid Andri Yulison <hadiidandri2000@gmail.com>
2025-11-03 12:42:38 +0700

    backedn hello world laravel

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -6,3 +6,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::get('/hello-world', function () {
+    return response()->json(['message' => 'Hello World!']);
+});
